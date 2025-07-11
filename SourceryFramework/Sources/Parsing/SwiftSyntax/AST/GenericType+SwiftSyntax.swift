20adeafd2a6849c730fb5560ce23f55176d07cd4
Lars <134181853+bo2themax@users.noreply.github.com>
2025-07-11 18:56:51 +0200

    fix compiling with Xcode 26

--- SourceryFramework/Sources/Parsing/SwiftSyntax/AST/GenericType+SwiftSyntax.swift
+++ SourceryFramework/Sources/Parsing/SwiftSyntax/AST/GenericType+SwiftSyntax.swift
@@ -4,9 +4,21 @@ import SwiftSyntax
 
 extension GenericType {
     convenience init(name: String, node: GenericArgumentClauseSyntax) {
+        #if canImport(FoundationModels)
+        // TODO: ExprSyntax may need to be handled
+        let parameters = node.arguments.map { argument -> GenericTypeParameter? in
+            switch argument.argument {
+            case .type(let type):
+                return GenericTypeParameter(typeName: TypeName(type))
+            case .expr:
+                return nil
+            }
+        }.compactMap({ $0 })
+        #else
         let parameters = node.arguments.map { argument in
             GenericTypeParameter(typeName: TypeName(argument.argument))
         }
+        #endif
 
         self.init(name: name, typeParameters: parameters)
     }
